95ad8fff0a13e2f2058587a1e2e4aa7b7b10c924
Erica Gurung <egurung@student.unimelb.edu.au>
2024-07-04 10:33:26 +1000

    deployment fixes

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,22 +1,22 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
-const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-      isDone: a.boolean(),  // Added new field
-    })
-    .authorization((allow) => [allow.owner()]),
-});
-
-export type Schema = ClientSchema<typeof schema>;
-
-// export const data = defineData({
-//   schema,
-//   authorizationModes: {
-//     defaultAuthorizationMode: "userPool",
-//     // API Key is used for a.allow.public() rules
-//     apiKeyAuthorizationMode: {
-//       expiresInDays: 30,
-//     },
-//   },
+// import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+// const schema = a.schema({
+//   Todo: a
+//     .model({
+//       content: a.string(),
+//       isDone: a.boolean(),  // Added new field
+//     })
+//     .authorization((allow) => [allow.owner()]),
 // });
+
+// export type Schema = ClientSchema<typeof schema>;
+
+// // export const data = defineData({
+// //   schema,
+// //   authorizationModes: {
+// //     defaultAuthorizationMode: "userPool",
+// //     // API Key is used for a.allow.public() rules
+// //     apiKeyAuthorizationMode: {
+// //       expiresInDays: 30,
+// //     },
+// //   },
+// // });
